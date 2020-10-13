a55ad620f2a8fc571881ee9c5486db48241f4cbb
Juan Jose Cazarez <juanjose.cazarezg@gmail.com>
2020-10-13 17:52:32 -0500

    Ya se lee el path del file

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ app = Flask(__name__)
 def index():
     #En este if checo la elección de los algoritmos
     if request.method == 'POST':
+        #Aqui se obtiene el path del archivo
+        f = request.files['file']
+        if f.filename != '':
+            print(f.filename)
+        else:
+            print("Seleccione un archivo")
         #Al presionar el botón de testear se obtiene el valor elegido de los algortimos
         print(request.form.get('algs'))
-        return render_template('index.html')
     return render_template('index.html')
 
 if __name__ == '__main__':
