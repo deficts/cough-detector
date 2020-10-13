efa18774a37b944c8eb98cf1c803a32054ba9d86
Juan Jose Cazarez <juanjose.cazarezg@gmail.com>
2020-10-13 17:00:53 -0500

    Cambios con botones de algoritmos

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def index():
+    #En este if checo la elección de los algoritmos
+    if request.method == 'POST':
+        #Al presionar el botón de testear se obtiene el valor elegido de los algortimos
+        print(request.form.get('algs'))
+        return render_template('index.html')
     return render_template('index.html')
 
 if __name__ == '__main__':
